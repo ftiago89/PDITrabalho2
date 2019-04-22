9ef128a148a5377a7f038736435165bb274fbb1e
Felipe Melo <ftiagomelo89@gmail.com>
2019-04-22 18:15:40 -0300

    Resolução de bug na classe Principal
    O array que estava sendo plotado depois de zerar os n cossenos mais importantes era o array errado.

--- src/trabalho2pdi/Principal.java
+++ src/trabalho2pdi/Principal.java
@@ -21,40 +21,47 @@ public class Principal {
         
         Scanner scan = new Scanner(System.in);
         System.out.print("Qual questão? ");
-        int questao = Integer.valueOf(scan.next());
         
-        //TRATAMENTO DAS QUESTOES
-        switch(questao){
-            //questao 1
-            case 1:
-                System.out.print("Valor de n para corte dos cossenos: ");
-                int n = Integer.valueOf(scan.next());
-                runDct = new RunDCT(samples[0]);//passa pro dominio da frequencia
-                resultadoTemp = runDct.getResultadoFinal();//pega o array de cossenos
-                cortaCossenos = new CortaCossenos(resultadoTemp, n);//aplica um corte dos n cossenos mais importantes e zera os demais
-                grafico.plotar(resultadoTemp, n+"cossenosmaisimportantes.png", "X[k] com "+n+" Cossenos mais Importantes", "k");//plota o grafico de X[k]
-                runIDct = new RunIDCT(resultadoTemp);//dct inversa pra voltar da frequencia pro espaco
-                double[] sinalVolta = runIDct.getResultadoFinal();//pega o sinal modificado
-                grafico.plotar(sinalVolta, "voltacom"+n+"cossenosmaisimportantes.png", "x[n] com "+n+" Cossenos mais Importantes", "n");//plota o grafico de x[n]
-                break;
-            //questao 2
-            case 2:
-                //fazer
-                break;
-            //questao 3
-            case 3:
-                System.out.print("Valor de c para deslocamento: ");
-                int c = Integer.valueOf(scan.next());
-                if(resultadoTemp == null){//se a questao 1 ja tiver sido feita, nao precisa passar de novo pra frequencia o sinal original
-                    runDct = new RunDCT(samples[0]);
-                    resultadoTemp = runDct.getResultadoFinal();
-                }
-                deslocaFrequencia = new DeslocaFrequencia(resultadoTemp, c);//aplica um deslocamento no array das frequencias
-                grafico.plotar(resultadoTemp, c+"cossenosdeslocados.png", "X[k] = X[k+"+c+"]", "k");//plota o grafico de X[k] deslocado
-                runIDct = new RunIDCT(resultadoTemp);//dct inversa para voltar da frequencia para o espaco os X[k] deslocado
-                double[] sinalDeslocado = runIDct.getResultadoFinal();//pega o array resultado da dct inversa
-                grafico.plotar(sinalDeslocado, "sinaldeslocadoem"+c+".png", "x[n] após deslocamento "+c, "n");//plota grafico de x[n] deslocado
-                break;
+        
+        while(true){
+            int questao = Integer.valueOf(scan.next());
+
+            //TRATAMENTO DAS QUESTOES
+            switch (questao) {
+                //questao 1
+                case 1:
+                    System.out.print("Valor de n para corte dos cossenos: ");
+                    int n = Integer.valueOf(scan.next());
+                    runDct = new RunDCT(samples[0]);//passa pro dominio da frequencia
+                    if (resultadoTemp == null) {//se a questao 1 ja tiver sido feita, nao precisa passar de novo pra frequencia o sinal original
+                        runDct = new RunDCT(samples[0]);
+                        resultadoTemp = runDct.getResultadoFinal();
+                    }
+                    cortaCossenos = new CortaCossenos(resultadoTemp, n);//aplica um corte dos n cossenos mais importantes e zera os demais
+                    grafico.plotar(cortaCossenos.getSaida(), n + "cossenosmaisimportantes.png", "X[k] com " + n + " Cossenos mais Importantes", "k");//plota o grafico de X[k]
+                    runIDct = new RunIDCT(cortaCossenos.getSaida());//dct inversa pra voltar da frequencia pro espaco
+                    double[] sinalVolta = runIDct.getResultadoFinal();//pega o sinal modificado
+                    grafico.plotar(sinalVolta, "voltacom" + n + "cossenosmaisimportantes.png", "x[n] com " + n + " Cossenos mais Importantes", "n");//plota o grafico de x[n]
+                    break;
+                //questao 2
+                case 2:
+                    //fazer
+                    break;
+                //questao 3
+                case 3:
+                    System.out.print("Valor de c para deslocamento: ");
+                    int c = Integer.valueOf(scan.next());
+                    if (resultadoTemp == null) {//se a questao 1 ja tiver sido feita, nao precisa passar de novo pra frequencia o sinal original
+                        runDct = new RunDCT(samples[0]);
+                        resultadoTemp = runDct.getResultadoFinal();
+                    }
+                    deslocaFrequencia = new DeslocaFrequencia(resultadoTemp, c);//aplica um deslocamento no array das frequencias
+                    grafico.plotar(resultadoTemp, c + "cossenosdeslocados.png", "X[k] = X[k+" + c + "]", "k");//plota o grafico de X[k] deslocado
+                    runIDct = new RunIDCT(resultadoTemp);//dct inversa para voltar da frequencia para o espaco os X[k] deslocado
+                    double[] sinalDeslocado = runIDct.getResultadoFinal();//pega o array resultado da dct inversa
+                    grafico.plotar(sinalDeslocado, "sinaldeslocadoem" + c + ".png", "x[n] após deslocamento " + c, "n");//plota grafico de x[n] deslocado
+                    break;
+            }
         }
-    } 
+    }
 }
